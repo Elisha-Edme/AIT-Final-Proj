b75a51c7f469a0c3ec70d0226209c0be25a59f8c
Elisha-Edme <eae8374@nyu.edu>
2023-12-11 17:33:29 -0500

    Fixed the server

--- server/routes/purchaseRoutes.js
+++ server/routes/purchaseRoutes.js
@@ -12,15 +12,10 @@ router.get('/user/:uid', async (req, res) => {
         const lst = [];
         const getDetails = async (ind) => {
             if (ind < user['purchases'].length) {
-                console.log('lst', lst);
                 const pid = user['purchases'][ind];
-                console.log('pi', pid);
                 const api_url = "https://finance-tracker-api-elisha-edmes-projects.vercel.app/api/purchases/"
-                //const api_url = "http://localhost:8080/api/purchases/"
                 await axios.get(`${api_url}${pid}`).then(async details => {
-                    console.log(details.data);
                     lst.push(details.data);
-                    console.log('lst2', lst);
                     await getDetails(ind + 1);
                 }).catch(console.log);
             }
@@ -37,9 +32,7 @@ router.get('/user/:uid', async (req, res) => {
 router.get('/:pid', async (req, res) => {
     const user = await Purchase.findById({_id: req.params.pid});
     if (user) {
-        console.log(user);
         res.status(200).json(user);
-        console.log(user);
     } else {
         res.status(404).json({ message: 'Purchase not found!'});
     }
@@ -48,7 +41,6 @@ router.get('/:pid', async (req, res) => {
 router.post('/addPurchase/:_id', async (req, res) => {
     try {
         const userId = req.params._id;
-        console.log(userId);
         // Check if userId is defined and is a valid ObjectId
         if (!userId || !mongoose.Types.ObjectId.isValid(userId)) {
             return res.status(400).json({ message: "Invalid user ID" });
@@ -76,26 +68,4 @@ router.post('/addPurchase/:_id', async (req, res) => {
     }
 });
 
-
-// router.post('/register', async (req, res) => {
-//     let {name, password} = req.body;
-//     name = sanitize(name);
-//     password = sanitize(password);
-//     // if a user is already in the db with that username
-//     const user = await User.findOne({name: name});
-//     if (user) {
-//         res.status(201).json({ message: `${name} is already a user. Please use a different name` });
-//     }
-//     else {
-//         const salt = await bcrypt.genSalt();
-//         const hash = await bcrypt.hash(password, salt);
-//         const newUser = await User.create({ name:name, password: hash });
-//         // Save the user to the database
-//         const token = createToken(newUser._id);
-
-//         res.status(200).json({name, token});
-//     }
-// });
-
-// router.get('/register', (req, res) => res.status(200).json({message: "shit works ong"}))
 module.exports = router;
